b57a6d85d5ab9e64341a4fe4c05b58264569ac23
Alexandra Eline Pettersen <57686601+frontalexandraep@users.noreply.github.com>
2021-12-02 23:03:26 +0100

    Complete

--- js/main.js
+++ js/main.js
@@ -144,12 +144,12 @@ const sr = ScrollReveal({
 
 /*==== Delay ====*/
 sr.reveal(`.home__data`);
-sr.reveal(`.home__img`, {delay: 500});
-sr.reveal(`.home__social`, {delay: 600});
+sr.reveal(`.home__img`, { delay: 500 });
+sr.reveal(`.home__social`, { delay: 600 });
 
 /*==== Origin ====*/
-sr.reveal(`.about__img, .contact__box`, {origin: 'left'});
-sr.reveal(`.about__data, .contact__form`, {origin: 'right'});
+sr.reveal(`.about__img, .contact__box`, { origin: "left" });
+sr.reveal(`.about__data, .contact__form`, { origin: "right" });
 
 /*==== Interval ====*/
-sr.reveal(`.steps__card, .product__card, .questions__group`, {interval: 100});
+sr.reveal(`.steps__card, .product__card, .questions__group`, { interval: 100 });
